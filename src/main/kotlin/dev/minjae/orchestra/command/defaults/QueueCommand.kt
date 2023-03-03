695e1294bc00904f878fbd1602534feda3d6fc50
alvin0319 <alvin7810@naver.com>
2023-03-03 20:58:42 +0900

    QueueCommand: Implement pagination

--- src/main/kotlin/dev/minjae/orchestra/command/defaults/QueueCommand.kt
+++ src/main/kotlin/dev/minjae/orchestra/command/defaults/QueueCommand.kt
@@ -7,11 +7,26 @@ import dev.minjae.orchestra.util.replyEphemeral
 import dev.minn.jda.ktx.messages.Embed
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import kotlin.math.ceil
+import kotlin.math.max
 
 class QueueCommand(bot: Bot) : BaseCommand(bot, "queue", "Show the current queue") {
+
+    init {
+        options.add(
+            OptionData(OptionType.INTEGER, "page", "The page of the queue")
+        )
+    }
+
     override fun execute(event: GenericCommandInteractionEvent) {
         val channel = event.messageChannel
         if (channel !is GuildMessageChannel) {
+            return
+        }
+        var page = event.getOption("page")?.asInt ?: run {
+            event.replyEphemeral("Please provide a page number").queue(null) {}
             return
         }
         PlayerDataStore.getPlayerData(channel.guild).let { playerData ->
@@ -19,17 +34,28 @@ class QueueCommand(bot: Bot) : BaseCommand(bot, "queue", "Show the current queue
                 event.replyEphemeral("There is no tracks in queue").queue(null) {}
             } else {
                 val queue = playerData.tracks
+                val maxPage = max(1, ceil((queue.size / 5).toDouble()).toInt())
+                if (page > maxPage) {
+                    page = maxPage
+                }
+                val tracks = queue.toList().subList((page - 1) * 5, minOf(page * 5, queue.size))
+                if (tracks.isEmpty()) {
+                    // should never happen I guess, doesn't it?
+                    event.replyEphemeral("There is no tracks in queue").queue(null) {}
+                    return
+                }
                 event.replyEmbeds(
                     Embed {
-                        title = "Queue"
+                        title = "Track Queue"
                         val desc = StringBuilder()
                         desc.appendln("Current playing: ${playerData.audioPlayer.playingTrack.info.title}")
-                        var position = 0
-                        for (track in queue) {
-                            position++
-                            desc.appendln("$position. ${track.info.title}")
+                        tracks.withIndex().forEach { (index, track) ->
+                            desc.appendln("${index + 1}. ${track.info.title}")
                         }
                         description = desc.toString()
+                        footer {
+                            name = "Page $page/$maxPage"
+                        }
                     }
                 ).queue(null) {}
             }
